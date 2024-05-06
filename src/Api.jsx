1049df191af78c61682ad517c7265d1b9d31327e
Supriya Bhandari <153041437+creativeall@users.noreply.github.com>
2024-05-06 16:52:25 +0530

    Create Api.jsx

---
+++ src/Api.jsx
@@ -0,0 +1,44 @@
+export const questions = [
+    {
+        id: 1,
+        question: 'How to create components in ReactJS?',
+        answer: 'Components are independent and reusable bits of code. They serve the same purpose as JavaScript functions, but work in isolation and return HTML.Components come in two types, Class components and Function components, in this tutorial we will concentrate on Function components.',
+    },
+    {
+        id: 2,
+        question: 'What is React?',
+        answer: 'React (aka React.js or ReactJS) is an open-source front-end JavaScript library that is used for building composable user interfaces, especially for single-page applications. It is used for handling view layer for web and mobile apps based on components in a declarative approach.React was created by Jordan Walke, a software engineer working for Facebook. React was first deployed on Facebooks News Feed in 2011 and on Instagram in 2012.',
+    },
+    {
+        id: 3,
+        question: 'What are props in React?',
+        answer: 'Props are inputs to components. They are single values or objects containing a set of values that are passed to components on creation similar to HTML-tag attributes. Here, the data is passed down from a parent component to a child component.',
+    },
+    {
+        id: 4,
+        question: 'What is useState() in React?',
+        answer: 'The useState() is a built-in React Hook that allows you for having state variables in functional components. It should be used when the DOM has something that is dynamically manipulating/controlling.',
+    },
+    {
+        id: 5,
+        question: ' What are the differences between functional and class components?',
+        answer: 'Before the introduction of Hooks in React, functional components were called stateless components and were behind class components on a feature basis. After the introduction of Hooks, functional components are equivalent to class components.Before the introduction of Hooks in React, functional components were called stateless components and were behind class components on a feature basis. After the introduction of Hooks, functional components are equivalent to class components.',
+    },
+    {
+        id: 6,
+        question: 'What are the advantages of using React?',
+        answer: 'Use of Virtual DOM to improve efficiency,Gentle learning curve,SEO friendly,Reusable components,Huge ecosystem of libraries to choose from',
+    },
+    {
+        id: 7,
+        question: 'What are the limitations of React?',
+        answer: 'React is not a full-blown framework as it is only a library.The components of React are numerous and will take time to fully grasp the benefits of all.It might be difficult for beginner programmers to understand React.Coding might become complex as it will make use of inline templating and JSX.',
+    },
+    {
+        id: 8,
+        question: ' What is virtual DOM in React?',
+        answer: 'React uses Virtual DOM which is like a lightweight copy of the actual DOM(a virtual representation of the DOM). So for every object that exists in the original DOM, there is an object for that in React Virtual DOM. It is the same, but it does not have the power to directly change the layout of the document. Manipulating DOM is slow, but manipulating Virtual DOM is fast as nothing gets drawn on the screen.',
+    },
+
+    
+]
